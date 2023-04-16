17d186b6549b46242c4b409367ef0224c619798b
“Kathleen <kgmcdonnell@mail.usf.edu>
2023-04-16 15:40:35 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,92 @@
+import axios from "axios";
+
+function Header() {
+  return (
+    <header>
+      <a href="#">Home</a> | <a href="#posts-index">All posts</a> | <a href="#posts-new">New post</a>
+    </header>
+  );
+}
+
+function PostsNew() {
+  return (
+    <div id="posts-new">
+      <h1>New post</h1>
+      <form>
+        <p>
+          <label>
+            Title: <input type="text" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Body: <input type="text" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Image: <input type="file" />
+          </label>
+        </p>
+        <p>
+          <button type="submit">Submit</button>
+        </p>
+      </form>
+    </div>
+  );
+}
+
+function PostsIndex(props) {
+  console.log(props);
+  return (
+    <div id="posts-index">
+      <h1>All posts</h1>
+      {props.posts.map((post) => (
+        <div key={post.id} className="posts">
+          <h3>{post.title}</h3>
+          <img src={post.image} />
+          <p>{post.body}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+      <p>Copyright 2023</p>
+    </footer>
+  );
+}
+
+function Content() {
+  let posts = [];
+
+  const handleIndexPosts = () => {
+    axios.get("http://localhost:3000/posts.json").then(function (response) {
+      console.log(response);
+      posts = response.data;
+    });
+  };
+
+  return (
+    <div>
+      <PostsNew />
+      <PostsIndex posts={posts} />
+      <button onClick={handleIndexPosts}>Load All Posts</button>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
