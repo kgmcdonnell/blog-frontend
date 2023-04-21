0986dcbca1d9f9c831b411fdb32413d2c5069b1c
“Kathleen <kgmcdonnell@mail.usf.edu>
2023-04-20 20:04:42 -0400

    added a function to create a new post

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,6 +1,10 @@
 import axios from "axios";
 import { useState } from "react";
 
+const jwt = localStorage.getItem("jwt");
+if (jwt) {
+  axios.defaults.headers.common["Authorization"] = `Bearer ${jwt}`;
+}
 export function Login() {
   const [errors, setErrors] = useState([]);
   const handleSubmit = (event) => {
--- src/PostsNew.jsx
+++ src/PostsNew.jsx
@@ -1,21 +1,33 @@
+import axios from "axios";
+
 export function PostsNew() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const params = new FormData(event.target);
+    axios.post("http://localhost:3000/posts", params).then(function (response) {
+      console.log(response);
+      event.target.reset();
+      window.location.href = "/";
+    });
+  };
+
   return (
     <div id="posts-new">
       <h1>New post</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <p>
           <label>
-            Title: <input type="text" />
+            Title: <input name="title" type="text" />
           </label>
         </p>
         <p>
           <label>
-            Body: <input type="text" />
+            Body: <input name="body" type="text" />
           </label>
         </p>
         <p>
           <label>
-            Image: <input type="file" />
+            Image: <input name="image" type="text" />
           </label>
         </p>
         <p>
